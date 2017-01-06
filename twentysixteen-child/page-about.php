bbdffb2dbc25fdd9f2a3fc61df6b4dfcacbeda07
Daniel-Bishop-student <s3299003@student.rmit.edu.au>
2017-01-06 11:28:46 +1100

    finished home page portrait and started work on about page

--- twentysixteen-child/page-about.php
+++ twentysixteen-child/page-about.php
@@ -16,10 +16,28 @@ get_header(); ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
+		<!--New-->
+					<div class="container">
+						<div class="row">
+							<div class="col-sm-12 about-top-image">
+
+								<h1 class="page-title-custom">About</h1>
+
+							</div>
+						</div>
+
+
+					</div>
+				<!--end new-->
 		<div class="about-inner-main">
+			
+
+
 			<div class="about-page-col1">
-				<h1 class="page-title-custom">About</h1>
-					<img src ="<?php echo get_stylesheet_directory_uri();?>/images/me.jpg" alt="pic-of-me" /> 
+
+				
+				
+					
 
 				<p>
 					<img class="contact-icon" src="<?php echo get_stylesheet_directory_uri();?>/images/svgsDanCorporate/wassap-logo-button.svg" alt="phone-icon"/>
@@ -31,40 +49,14 @@ get_header(); ?>
 				</p>
 			</div>
 			<div class= "about-page-col2">
-				<h2>This is me</h2>
+				
 			
 				<p>
-					I love my music, and I love web development.  
-					I find music and web design very similar in that they are both 
-					creative and are about creating rhythm (or layout), 
-					color and excitement (melody) and connecting with people on a 
-					sub-concious level.
+					I design and develop professional websites for professional people and businesses.  
+					Strong branding and online presence are a must these days and a website helps businesses achieve
+					these goals and reach wider audiences than most other media.
 				</p>
-				<p>
-					Music and web development are hard work and incredibly 
-					frustrating at times.  They both take you on a life long 
-					journey of learning and there is always a new challenge and 
-					area of knowledge to improve in.
-
-				</p>
-
-				<p>
-					The rewards in both cases is presenting your creation to the 
-					world and knowing you have done it well.  I know my job is done
-					 well when the message is communicated and the intended audience 
-					 is reached.  The customers are happy and have had a 
-					 great experience through my creations.  
-				</p>
-
-				<p>
-					Being a musician and a web developer often requires 
-					collaboration and co-operation.  This is also exciting because 
-					you get to create and reach people as a team and share the 
-					experience with your bandmates or fellow collaborators, 
-					whether they are clients or development teams.
-
-				</p>
-
+				
 				<p>
 					So come with me and lets create a song (or web experience) together.
 
